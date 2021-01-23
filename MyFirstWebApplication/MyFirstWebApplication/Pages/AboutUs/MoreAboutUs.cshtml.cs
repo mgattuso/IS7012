38ae6207d5e7af725ceb2276785cc408954787b6
Michael Gattuso <michael.gattuso@gmail.com>
2021-01-23 12:57:08 -0500

    First Version

---
+++ MyFirstWebApplication/MyFirstWebApplication/Pages/AboutUs/MoreAboutUs.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstWebApplication.Pages.AboutUs
+{
+    public class MoreAboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
